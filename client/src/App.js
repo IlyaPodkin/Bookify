a20d8efe05a5db15054fe8f613ab82bbf4d99d56
IlyaPodkin <ilhapodkin@yandex.ru>
2025-02-25 17:17:41 +0400

    Реализация страницы регистрации

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles/main.css";
 import Authorization from "./components/Authorization";
-// import Registration from "./components/Registration"
+import Registration from "./components/Registration"
 
 function App() {
   return (
     <div className="main">
-      <Authorization></Authorization>
-      {/* <Registration></Registration> */}
+      {/* <Authorization></Authorization> */}
+      <Registration></Registration>
     </div>
   );
 }
--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -2,18 +2,19 @@ import "../styles/registration.css";
 
 const Registration = () => {
     return (
-        <div className="authorization-container">
-            <h1 className="authorization-header">Авторизация</h1>
-            <div>
+        <div className="registration-container">
+            <h1 className="registration-header">Регистрация</h1>
+            <div className="input-container">
                 <p>E-mail</p>
                 <input type="text" className="login-input" />
                 <p>Пароль</p>
                 <input type="password" className="password-input" />
+                <p>Повторите пароль</p>
+                <input type="password" className="password-input" />
             </div>
             <button className="button-sign-in">Войти</button>
             <div className="link-container">
-                <a className="link-password-recovery" href="">Забыли пароль?</a>
-                <a className="link-sign-up" href="">Регистрация</a>
+                <a className="link-sign-up" href="">Авторизация</a>
             </div>
         </div>
     )
